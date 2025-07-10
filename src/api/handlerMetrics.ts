3af46ecab79fb2ba1f04e595fb7dcb2b07479129
philipreese <pbreese42@gmail.com>
2025-07-10 16:10:58 -0400

    move metrics and reset to admin namespace and return html for metrics

--- src/api/handlerMetrics.ts
+++ src/api/handlerMetrics.ts
@@ -2,7 +2,13 @@ import type { Request, Response } from "express";
 import { apiConfig } from "../config.js";
 
 export async function handlerMetrics(_: Request, res: Response) {
-    res.set("Content-Type", "text/plain; charset=utf-8");
-    res.send(`Hits: ${apiConfig.fileserverHits}`);
+    res.set("Content-Type", "text/html; charset=utf-8");
+    res.send(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${apiConfig.fileserverHits} times!</p>
+  </body>
+</html>`);
     res.end();
 }
