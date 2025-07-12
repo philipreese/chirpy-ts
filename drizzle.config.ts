8457724276a79c6150a79304cbfc005b263ef3d7
philipreese <pbreese42@gmail.com>
2025-07-12 11:49:00 -0400

    create user query, add db url from env, add connection to database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { apiConfig } from "./src/config.js";
 
 export default defineConfig({
     schema: "src/db/schema.ts",
     out: "src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
-        url: "postgres://postgres:postgres@localhost:5432/chirpy_ts?sslmode=disable",
+        url: apiConfig.dbURL,
     },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,18 @@
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 };
+
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+    if (!process.env[key]) {
+        throw new Error(`Environment variable ${key} is not set`);
+    }
+    return process.env[key];
+}
 
 export const apiConfig: APIConfig = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
