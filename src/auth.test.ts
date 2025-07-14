602c4aefb2bf6d0bbb28033a44a3f2e0b48f6f23
philipreese <pbreese42@gmail.com>
2025-07-14 12:58:29 -0400

    add bility to create and verify JWTs

---
+++ src/auth.test.ts
@@ -0,0 +1,50 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import { checkPasswordHash, hashPassword, makeJWT, validateJWT } from "./auth";
+import { UnauthorizedError } from "./api/errors";
+
+describe("Password Hashing", () => {
+    const password1 = "correctPassword123!";
+    const password2 = "anotherPassword456!";
+    let hash1: string;
+    let hash2: string;
+
+    beforeAll(async () => {
+        hash1 = await hashPassword(password1);
+        hash2 = await hashPassword(password2);
+    });
+
+    it("should return true for the correct password", async () => {
+        const result = await checkPasswordHash(password1, hash1);
+        expect(result).toBe(true);
+    });
+
+    it("should return false for incorrect password", async () => {
+        const result = await checkPasswordHash(password2, hash1);
+        expect(result).toBe(false);
+    });
+});
+
+describe("Json Web Tokens", () => {
+    const secret = "1234";
+    const badSecret = "9876";
+    const badToken = "token.user.1234";
+    const userId = "userId!";
+    let token: string;
+
+    beforeAll(async () => {
+        token = makeJWT(userId, 3600, secret);
+    });
+
+    it("should validate a valid token", () => {
+        const result = validateJWT(token, secret);
+        expect(result).toBe(userId);
+    });
+
+    it("should throw error for invalid token", () => {
+        expect(() => validateJWT(badToken, secret)).toThrow(UnauthorizedError);
+    });
+
+    it("should throw error when token is signed with wrong secret", () => {
+        expect(() => validateJWT(token, badSecret)).toThrow(UnauthorizedError);
+    });
+});
--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,8 @@
 import bcrypt from "bcrypt";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { UnauthorizedError } from "./api/errors";
+
+const TOKEN_ISSUER = "chirpy";
 
 export async function hashPassword(password: string) {
     return await bcrypt.hash(password, 10);
@@ -6,4 +10,44 @@ export async function hashPassword(password: string) {
 
 export async function checkPasswordHash(password: string, hash: string) {
     return await bcrypt.compare(password, hash);
+}
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function makeJWT(
+    userID: string,
+    expiresIn: number,
+    secret: string
+): string {
+    const issuedAt = Math.floor(Date.now() / 1000);
+
+    return jwt.sign(
+        {
+            iss: TOKEN_ISSUER,
+            sub: userID,
+            iat: issuedAt,
+            exp: issuedAt + expiresIn,
+        } satisfies payload,
+        secret
+    );
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    let decoded: payload;
+
+    try {
+        decoded = jwt.verify(tokenString, secret) as JwtPayload;
+    } catch (error) {
+        throw new UnauthorizedError("Invalid token");
+    }
+
+    if (decoded.iss != TOKEN_ISSUER) {
+        throw new UnauthorizedError("Invalid issuer");
+    }
+
+    if (!decoded.sub) {
+        throw new UnauthorizedError("No user ID in token");
+    }
+
+    return decoded.sub;
 }
